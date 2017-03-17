0e55e9fc8db5804104d6382e8fa12f7dad2ef3b9
Juan Carlos Ferrel <juancarlos.ferrel@Mac-mini-de-Juan.local>
2017-03-17 18:52:17 -0400

    Ionic 2 tutorial with Typescript Angular 2

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { UsersPage } from '../pages/users/users';
+import { ReposPage } from '../pages/repos/repos';
+import { OrganizationsPage } from '../pages/organizations/organizations';
+import { GithubUsers } from '../providers/github-users';
+import { UserDetailsPage } from '../pages/user-details/user-details';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    UsersPage,
+    ReposPage,
+    OrganizationsPage,
+    UserDetailsPage,
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    UsersPage,
+    ReposPage,
+    OrganizationsPage,
+    UserDetailsPage
+  ],
+  providers: [GithubUsers]
+})
+export class AppModule {}
